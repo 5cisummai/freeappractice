927665bf23aff50cfb30c31a6535f50643dc2552
5cisummai <ajay.saravanan@outlook.com>
2026-08-09 17:27:59 -0700

    Remove temporary Super landing page redirect to pricing

--- src/routes/(marketing)/super/+page.server.ts
+++
@@ -1,7 +0,0 @@
-import { redirect } from '@sveltejs/kit';
-import type { PageServerLoad } from './$types';
-
-// Temporary: send /super to pricing until the Super landing is ready.
-export const load: PageServerLoad = () => {
-	throw redirect(302, '/pricing');
-};
